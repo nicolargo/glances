f2653a95dbfb5a8f7f72fe5c4386aabc64878d2f
Nicolas Hennion <nicolas@nicolargo.com>
2012-06-21 22:30:15 +0200

    A bunch of new features (#48) by Alessio Sergi

--- glances/glances.py
+++ glances/glances.py
@@ -22,7 +22,7 @@
 from __future__ import generators
 
 __appname__ = 'glances'
-__version__ = "1.4b19"
+__version__ = "1.4b20"
 __author__ = "Nicolas Hennion <nicolas@nicolargo.com>"
 __licence__ = "LGPL"
 
