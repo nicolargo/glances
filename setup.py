6310e1ef5fed8d0251e9db15a8cdd8e883ac5939
Nicolas Hennion <nicolas@nicolargo.com>
2013-07-07 17:01:01 +0200

    Add batinfo to setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=['psutil>=0.4.1'],
     extras_require={
         'HTML': ['jinja2>=2.0'],
-        'SENSORS': ['pysensors>=0.0.2']
+        'SENSORS': ['pysensors>=0.0.2'],
         'BATINFO': ['batinfo>=0.1.3']
     },
     packages=['glances'],
