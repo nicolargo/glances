3fd75525207eb13105e7bd1f4daed913ae76131d
Nicolas Hennion <nicolas@nicolargo.com>
2013-08-10 10:22:26 +0200

    Add compatibility with pip >= 1.4 / Issue #270

--- setup.py
+++ setup.py
@@ -30,13 +30,13 @@ for mo in glob.glob('i18n/*/LC_MESSAGES/*.mo'):
 
 setup(
     name='Glances',
-    version='1.7a',
+    version='1.7',
     description="A cross-platform curses-based monitoring tool",
     long_description=open('README.rst').read(),
     author='Nicolas Hennion',
     author_email='nicolas@nicolargo.com',
     url='https://github.com/nicolargo/glances',
-    download_url='https://s3.amazonaws.com/glances/glances-1.7a.tar.gz',
+    # download_url='https://s3.amazonaws.com/glances/glances-1.7a.tar.gz',
     license="LGPL",
     keywords="cli curses monitoring system",
     install_requires=['psutil>=0.4.1'],
