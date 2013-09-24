4cac5f1f94cb6535947132aeebaaa76aa42cf97a
Alessio Sergi <al3hex@gmail.com>
2013-09-24 20:00:00 +0200

    Fix issue with python 3.x on Windows [issue #291]

--- glances/glances.py
+++ glances/glances.py
@@ -116,7 +116,6 @@ if is_Windows:
 
             def stop(self):
                 self.Terminated = True
-                msvcrt.putch(' ')
                 while not self.q.empty():
                     self.q.get()
 
