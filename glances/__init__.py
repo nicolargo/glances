9335672b05de0001e28a88d599f3ff65c551a56c
Nicolas Hennion <nicolas@nicolargo.com>
2014-01-12 20:56:03 +0100

    Dev branch

---
+++ glances/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Glances - An eye on your system
+#
+# Copyright (C) 2014 Nicolargo <nicolas@nicolargo.com>
+#
+# Glances is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Glances is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+__appname__ = 'glances'
+__version__ = "2.0_Alpha"
+__author__ = "Nicolas Hennion <nicolas@nicolargo.com>"
+__license__ = "LGPL"
+
+from .core.glances_core import GlancesCore
+
+def main(argv=None):
+    print "!!! %s" % argv
+    glances_instance = GlancesCore()
+    glances_instance.start()
