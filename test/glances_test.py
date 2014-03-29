ec604a5c4ee477b15891c4c80b97293a9cb6d1d7
Nicolas Hennion <nicolas@nicolargo.com>
2014-03-29 09:50:20 +0100

    Unitary test(bed)

---
+++ test/glances_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Glances - An eye on your system
+#
+# Copyright (C) 2014 Nicolargo <nicolas@nicolargo.com>
+#
+# Glances is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Glances is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+Tests suite...
+"""
+
+import unittest
+import glances
+
+class testGlances(unittest.TestCase):
+    """
+    Test glances class
+    """
+
+    def test_init(self):
+        """
+        ...
+        """
+        self.assertEqual(0, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
