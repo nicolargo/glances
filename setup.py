8d895f1845c198d0737c65489d7573fb087a4ca2
Alessio Sergi <al3hex@gmail.com>
2014-05-11 20:00:00 +0200

    Add Python 3.4 support
    Add Python 3.4 to .travis.yml.

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
     ]
 )
