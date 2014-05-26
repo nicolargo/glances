7b260f3e12a4f544bbef36b04b4074d2f4c1f402
Nicolargo <nicolas@nicolargo.com>
2014-05-26 15:47:21 +0200

    Ready for RC1...

--- glances/core/glances_globals.py
+++ glances/core/glances_globals.py
@@ -19,7 +19,7 @@
 
 # Glances informations
 __appname__ = 'glances'
-__version__ = '2.0_Alpha02'
+__version__ = '2.0_RC1'
 __author__ = 'Nicolas Hennion <nicolas@nicolargo.com>'
 __license__ = 'LGPL'
 
