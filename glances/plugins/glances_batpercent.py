51a3c4171eb03641bf1f0104892e204386a5d2f9
Nicolargo <nicolas@nicolargo.com>
2014-07-05 09:15:15 +0200

    Batinfo message

--- glances/plugins/glances_batpercent.py
+++ glances/plugins/glances_batpercent.py
@@ -19,15 +19,15 @@
 
 """Battery plugin."""
 
+# Import Glances libs
+from glances.core.glances_globals import logger
+from glances.plugins.glances_plugin import GlancesPlugin
+
 # Batinfo library (optional; Linux-only)
 try:
     import batinfo
 except ImportError:
-    pass
-
-# Import Glances libs
-from glances.core.glances_globals import logger
-from glances.plugins.glances_plugin import GlancesPlugin
+    logger.error(_("Cannot grab battery sensor. Missing BatInfo library."))
 
 
 class Plugin(GlancesPlugin):
@@ -85,7 +85,6 @@ class GlancesGrabBat(object):
             self.bat_list = []
             self.update()
         except Exception:
-            logger.error(_("Cannot grab battery sensor. Missing BatInfo library."))
             self.initok = False
 
     def update(self):
