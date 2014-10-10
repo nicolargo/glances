3ab089e96dfcf958be7935f9b56b74bd48ebaf90
Nicolas Hennion <nicolas@nicolargo.com>
2014-10-10 14:54:20 +0200

    Prepare repos for new version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def get_requires():
 
 setup(
     name='Glances',
-    version='2.1.2',
+    version='2.2_BETA',
     description="A cross-platform curses-based monitoring tool",
     long_description=open('README.rst').read(),
     author='Nicolas Hennion',
