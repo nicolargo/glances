d85e8b3f0933da1bfe9aa0129e5e592650012711
Nicolargo <nicolas@nicolargo.com>
2014-11-29 11:15:08 +0100

    Add patch for pip user install (thk to @asergi) (correct issue #383)

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import sys
 
 from setuptools import setup
 
+
 def get_data_files():
     data_files = [
         ('share/doc/glances', ['AUTHORS', 'COPYING', 'NEWS', 'README.rst',
@@ -14,20 +15,28 @@ def get_data_files():
         ('share/man/man1', ['man/glances.1'])
     ]
 
-    if hasattr(sys, 'real_prefix') or 'bsd' in sys.platform:
+    if os.name == 'posix' and os.getuid() == 0:  # Unix-like + root privileges
+        if 'bsd' in sys.platform:
+            conf_path = os.path.join(sys.prefix, 'etc', 'glances')
+        elif 'linux' in sys.platform:
+            conf_path = os.path.join('/etc', 'glances')
+        elif 'darwin' in sys.platform:
+            conf_path = os.path.join('/usr/local', 'etc', 'glances')
+    elif hasattr(sys, 'real_prefix'):  # virtualenv
         conf_path = os.path.join(sys.prefix, 'etc', 'glances')
-    elif not hasattr(sys, 'real_prefix') and 'linux' in sys.platform:
-        conf_path = os.path.join('/etc', 'glances')
-    elif 'darwin' in sys.platform:
-        conf_path = os.path.join('/usr/local', 'etc', 'glances')
-    elif 'win32' in sys.platform:
+    elif 'win32' in sys.platform:  # windows
         conf_path = os.path.join(os.environ.get('APPDATA'), 'glances')
+    else:  # Unix-like + per-user install
+        conf_path = os.path.join('etc', 'glances')
+
     data_files.append((conf_path, ['conf/glances.conf']))
 
     for mo in glob.glob('i18n/*/LC_MESSAGES/*.mo'):
-        data_files.append((os.path.dirname(mo).replace('i18n/', 'share/locale/'), [mo]))
+        data_files.append(
+            (os.path.dirname(mo).replace('i18n/', 'share/locale/'), [mo]))
 
     return data_files
+
 
 def get_requires():
     requires = ['psutil>=2.0.0']
