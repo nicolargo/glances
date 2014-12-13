f5747a5b00d308d5af10cb60d92c63a4868bef60
Nicolargo <nicolas@nicolargo.com>
2014-12-13 22:32:53 +0100

    Change version to 2.2

--- glances/__init__.py
+++ glances/__init__.py
@@ -20,7 +20,7 @@
 """Init the Glances software."""
 
 __appname__ = 'glances'
-__version__ = '2.2_BETA'
+__version__ = '2.2'
 __author__ = 'Nicolas Hennion <nicolas@nicolargo.com>'
 __license__ = 'LGPL'
 
