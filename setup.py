35bdaaa977bd2142c1a514ff3e3b95693b1941ab
Nicolargo <nicolas@nicolargo.com>
2015-05-04 11:32:22 +0200

    Commit Glances 2.4 RC1

--- setup.py
+++ setup.py
@@ -33,13 +33,13 @@ def get_requires():
 
 setup(
     name='Glances',
-    version='2.4beta',
+    version='2.4RC1',
     description="A cross-platform curses-based monitoring tool",
     long_description=open('README.rst').read(),
     author='Nicolas Hennion',
     author_email='nicolas@nicolargo.com',
     url='https://github.com/nicolargo/glances',
-    # download_url='https://s3.amazonaws.com/glances/glances-2.3.tar.gz',
+    # download_url='https://s3.amazonaws.com/glances/glances-2.4RC1.tar.gz',
     license="LGPL",
     keywords="cli curses monitoring system",
     install_requires=get_requires(),
