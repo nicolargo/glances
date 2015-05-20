31de5b3b1c48859b14fd9416cc359c77a4b1a3fe
nicolargo <nicolashennion@gmail.com>
2015-05-20 17:30:27 +0200

    On the road of Glances 2.5... the return

--- glances/__init__.py
+++ glances/__init__.py
@@ -20,7 +20,7 @@
 """Init the Glances software."""
 
 __appname__ = 'glances'
-__version__ = '2.4.1'
+__version__ = '2.5beta'
 __author__ = 'Nicolas Hennion <nicolas@nicolargo.com>'
 __license__ = 'LGPL'
 
