5bccb814392d9ecf12b410843b1e7330d0fb72d2
Nicolargo <nicolas@nicolargo.com>
2015-08-21 16:07:08 +0200

    Replace Timer by sleep

--- glances/plugins/glances_docker.py
+++ glances/plugins/glances_docker.py
@@ -472,14 +472,16 @@ class ThreadDockerGrabber(threading.Thread):
         """Function called to grab stats.
         Infinite loop, should be stopped by calling the stop() method"""
 
-        is_stopped = False
         for i in self._stats_stream:
             self._stats = i
-            countdown = Timer(self._refresh_time)
-            while not countdown.finished() and not is_stopped:
-                is_stopped = self.stopped()
-                time.sleep(0.1)
-            if is_stopped:
+            # countdown = Timer(self._refresh_time)
+            # while not countdown.finished() and not is_stopped:
+            #     is_stopped = self.stopped()
+            #     time.sleep(0.1)
+            # if is_stopped:
+            #     break
+            time.sleep(0.1)
+            if self.stopped():
                 break
 
     @property
