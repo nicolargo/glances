dc2ceed5a85ad693ad432f846d1ad1273c8f8744
nicolargo <nicolashennion@gmail.com>
2015-08-30 16:33:59 +0200

    Error with IP plugin (issue #651)

--- glances/plugins/glances_ip.py
+++ glances/plugins/glances_ip.py
@@ -65,15 +65,15 @@ class Plugin(GlancesPlugin):
             # Update stats using the netifaces lib
             try:
                 default_gw = netifaces.gateways()['default'][netifaces.AF_INET]
-            except KeyError:
-                logger.debug("Can not grab the default gateway")
+            except (KeyError, AttributeError) as e:
+                logger.debug("Can not grab the default gateway ({})".format(e))
             else:
                 try:
                     self.stats['address'] = netifaces.ifaddresses(default_gw[1])[netifaces.AF_INET][0]['addr']
                     self.stats['mask'] = netifaces.ifaddresses(default_gw[1])[netifaces.AF_INET][0]['netmask']
                     self.stats['mask_cidr'] = self.ip_to_cidr(self.stats['mask'])
                     self.stats['gateway'] = netifaces.gateways()['default'][netifaces.AF_INET][0]
-                except KeyError as e:
+                except (KeyError, AttributeError) as e:
                     logger.debug("Can not grab IP information (%s)".format(e))
 
         elif self.input_method == 'snmp':
