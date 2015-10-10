f7b4b718834590b59a39706f095af078d4306c4b
nicolargo <nicolashennion@gmail.com>
2015-10-10 11:34:03 +0200

    After 2.5.1...

--- glances/__init__.py
+++ glances/__init__.py
@@ -20,7 +20,7 @@
 """Init the Glances software."""
 
 __appname__ = 'glances'
-__version__ = '2.5.1'
+__version__ = '2.6beta'
 __author__ = 'Nicolas Hennion <nicolas@nicolargo.com>'
 __license__ = 'LGPL'
 
