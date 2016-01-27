nicolargo/glances

commit 9da6b3e49236f71b1d65f834a3c83a4465fd775d

Short process name displays arguments (issue #609)

glances/plugins/glances_processlist.py
@@ -280,13 +280,15 @@ class Plugin(GlancesPlugin):
         # If no command line for the process is available, fallback to
         # the bare process name instead
         cmdline = p['cmdline']
-        if cmdline == "" or args.process_short_name:
-            msg = ' {0}'.format(p['name'])
-            ret.append(self.curse_add_line(msg, splittable=True))
-        else:
-            try:
+        argument = ' '.join(cmdline.split()[1:])
+        try:
+            if cmdline == "" or args.process_short_name:
+                msg = ' {0}'.format(p['name'])
+                ret.append(self.curse_add_line(msg, decoration='PROCESS', splittable=True))
+                msg = ' {0}'.format(argument)
+                ret.append(self.curse_add_line(msg, splittable=True))
+            else:
                 cmd = cmdline.split()[0]
-                argument = ' '.join(cmdline.split()[1:])
                 path, basename = os.path.split(cmd)
                 if os.path.isdir(path):
                     msg = ' {0}'.format(path) + os.sep
@@ -295,10 +297,10 @@ class Plugin(GlancesPlugin):
                 else:
                     msg = ' {0}'.format(basename)
                     ret.append(self.curse_add_line(msg, decoration='PROCESS', splittable=True))
-                msg = " {0}".format(argument)
+                msg = ' {0}'.format(argument)
                 ret.append(self.curse_add_line(msg, splittable=True))
-            except UnicodeEncodeError:
-                ret.append(self.curse_add_line("", splittable=True))
+        except UnicodeEncodeError:
+            ret.append(self.curse_add_line("", splittable=True))
 
         # Add extended stats but only for the top processes
         # !!! CPU consumption ???

commit 252e80357134ac1094b7e8daeea93eb0b2c379b9

Hide program path in short process view
Fix bug introduced in commit 9da6b3e.

glances/plugins/glances_processlist.py
@@ -295,15 +295,10 @@ class Plugin(GlancesPlugin):
             if cmdline == '':
                 msg = ' {0}'.format(p['name'])
                 ret.append(self.curse_add_line(msg, splittable=True))
-            elif args.process_short_name:
-                msg = ' {0}'.format(p['name'])
-                ret.append(self.curse_add_line(msg, decoration='PROCESS', splittable=True))
-                msg = ' {0}'.format(argument)
-                ret.append(self.curse_add_line(msg, splittable=True))
             else:
                 cmd = cmdline.split()[0]
                 path, basename = os.path.split(cmd)
-                if os.path.isdir(path):
+                if os.path.isdir(path) and not args.process_short_name:
                     msg = ' {0}'.format(path) + os.sep
                     ret.append(self.curse_add_line(msg, splittable=True))
                     ret.append(self.curse_add_line(basename, decoration='PROCESS', splittable=True))