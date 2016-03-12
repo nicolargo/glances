ec920756c006e529a626c299d4bf60beaf7c7ff9
Alessio Sergi <al3hex@gmail.com>
2016-03-12 16:16:16 +0100

    Use insert for sys.path manipulations
    No longer import the wrong package.

--- glances/__main__.py
+++ glances/__main__.py
@@ -30,7 +30,7 @@ if __package__ is None and not hasattr(sys, "frozen"):
     # It is a direct call to __main__.py
     import os.path
     path = os.path.realpath(os.path.abspath(__file__))
-    sys.path.append(os.path.dirname(os.path.dirname(path)))
+    sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
 
 import glances  # noqa
 
