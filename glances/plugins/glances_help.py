96c63340f2d881d1b8936f08e3d67844bf28bb4e
Nicolas Hart <contact@nclshart.net>
2016-07-24 00:53:50 +0200

    Add ports shortcut to the help page

--- glances/plugins/glances_help.py
+++ glances/plugins/glances_help.py
@@ -94,6 +94,7 @@ class Plugin(GlancesPlugin):
         self.view_data['diskio_iops'] = msg_col2.format('B', 'Count/rate for Disk I/O')
         self.view_data['show_hide_top_menu'] = msg_col2.format('5', 'Show/hide top menu (QL, CPU, MEM, SWAP and LOAD)')
         self.view_data['show_hide_amp'] = msg_col2.format('A', 'Show/hide AMP plugin')
+        self.view_data['enable_disable_ports'] = msg_col.format('P', 'Enable/disable ports plugin')
         self.view_data['edit_pattern_filter'] = 'ENTER: Edit the process filter pattern'
 
     def get_view_data(self, args=None):
@@ -169,6 +170,8 @@ class Plugin(GlancesPlugin):
         ret.append(self.curse_new_line())
         ret.append(self.curse_add_line(self.view_data['enable_disable_irix']))
         ret.append(self.curse_add_line(self.view_data['quit']))
+        ret.append(self.curse_new_line())
+        ret.append(self.curse_add_line(self.view_data['enable_disable_ports']))
         ret.append(self.curse_new_line())
 
         ret.append(self.curse_new_line())
