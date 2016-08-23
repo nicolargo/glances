9650ce0d678093adf2f30bddc8a53f26c5b583e6
nicolargo <nicolashennion@gmail.com>
2016-08-23 20:12:49 +0200

    Change the IRQ shortcut key from R to Q. R is alread used for the RAID plugins. Also update the docs for the IRQ plugin. Last but not leat, change the ReadTheDoc theme

--- docs/conf.py
+++ docs/conf.py
@@ -115,15 +115,12 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'github_user': 'nicolargo',
-    'github_repo': 'glances',
-    'show_powered_by': False
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
