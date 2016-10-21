92b4e1adf0b881808ce881973c409b4425809682
nicolargo <nicolas@nicolargo.com>
2016-10-21 22:32:39 +0200

    Correct an issue in the UT for the new Wifi plugin

--- unitest-restful.py
+++ unitest-restful.py
@@ -94,7 +94,8 @@ class TestGlances(unittest.TestCase):
             if p in ('uptime', 'now'):
                 self.assertIsInstance(req.json(), text_type)
             elif p in ('fs', 'percpu', 'sensors', 'alert', 'processlist', 'diskio',
-                       'hddtemp', 'batpercent', 'network', 'folders', 'amps', 'ports', 'irq'):
+                       'hddtemp', 'batpercent', 'network', 'folders', 'amps', 'ports',
+                       'irq', 'wifi'):
                 self.assertIsInstance(req.json(), list)
             elif p in ('psutilversion', 'help'):
                 pass
