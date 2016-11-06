c23d3a255fd2b195e7e404bf32515577e3a68cac
nicolargo <nicolas@nicolargo.com>
2016-11-06 11:47:51 +0100

    Refactor Glances IRQ plugin

--- glances/plugins/glances_irq.py
+++ glances/plugins/glances_irq.py
@@ -22,6 +22,7 @@
 import os
 import operator
 
+from glances.logger import logger
 from glances.globals import LINUX
 from glances.timer import getTimeSinceLastUpdate
 from glances.plugins.glances_plugin import GlancesPlugin
@@ -34,8 +35,6 @@ class Plugin(GlancesPlugin):
     stats is a list
     """
 
-    IRQ_FILE = '/proc/interrupts'
-
     def __init__(self, args=None):
         """Init the plugin."""
         super(Plugin, self).__init__(args=args)
@@ -43,14 +42,13 @@ class Plugin(GlancesPlugin):
         # We want to display the stat in the curse interface
         self.display_curse = True
 
-        self.lasts = {}
-
         # Init the stats
+        self.irq = GlancesIRQ()
         self.reset()
 
     def get_key(self):
         """Return the key of the list."""
-        return 'irq_line'
+        return self.irq.get_key()
 
     def reset(self):
         """Reset/init the stats."""
@@ -65,37 +63,18 @@ class Plugin(GlancesPlugin):
         self.reset()
 
         # IRQ plugin only available on GNU/Linux
-        # Correct issue #947: IRQ file do not exist on OpenVZ container
-        if not LINUX \
-           or not os.path.exists(self.IRQ_FILE):
+        if not LINUX:
             return self.stats
 
         if self.input_method == 'local':
-            with open(self.IRQ_FILE) as irq_proc:
-                time_since_update = getTimeSinceLastUpdate('irq')
-                irq_proc.readline()  # skip header line
-                for irq_line in irq_proc.readlines():
-                    splitted_line = irq_line.split()
-                    irq_line = splitted_line[0].replace(':', '')
-                    current_irqs = sum([int(count) for count in splitted_line[
-                                       1:] if count.isdigit()])  # sum interrupts on all CPUs
-                    irq_rate = int(
-                        current_irqs -
-                        self.lasts.get(irq_line) if self.lasts.get(irq_line) else 0 //
-                        time_since_update)
-                    irq_current = {
-                        'irq_line': irq_line,
-                        'irq_rate': irq_rate,
-                        'key': self.get_key(),
-                        'time_since_update': time_since_update
-                    }
-                    self.stats.append(irq_current)
-                    self.lasts[irq_line] = current_irqs
+            # Grab the stats
+            self.stats = self.irq.get()
 
         elif self.input_method == 'snmp':
             # not available
             pass
 
+        # Get the TOP 5
         self.stats = sorted(self.stats, key=operator.itemgetter(
             'irq_rate'), reverse=True)[:5]  # top 5 IRQ by rate/s
 
@@ -132,9 +111,108 @@ class Plugin(GlancesPlugin):
 
         for i in self.stats:
             ret.append(self.curse_new_line())
-            msg = '{:>3}'.format(i['irq_line'])
+            msg = '{:<15}'.format(i['irq_line'])
             ret.append(self.curse_add_line(msg))
-            msg = '{:>20}'.format(str(i['irq_rate']))
+            msg = '{:>8}'.format(str(i['irq_rate']))
             ret.append(self.curse_add_line(msg))
 
         return ret
+
+
+class GlancesIRQ(object):
+    """
+    This class manages the IRQ file
+    """
+
+    IRQ_FILE = '/proc/interrupts'
+
+    def __init__(self):
+        """
+        Init the class
+        The stat are stored in a internal list of dict
+        """
+        self.lasts = {}
+        self.reset()
+
+    def reset(self):
+        """Reset the stats"""
+        self.stats = []
+        self.cpu_number = 0
+
+    def get(self):
+        """Return the current IRQ stats"""
+        return self.__update()
+
+    def get_key(self):
+        """Return the key of the dict."""
+        return 'irq_line'
+
+    def __header(self, line):
+        """The header contain the number of CPU
+
+        CPU0       CPU1       CPU2       CPU3
+        0:         21          0          0          0   IO-APIC   2-edge      timer
+        """
+        self.cpu_number = len(line.split())
+        return self.cpu_number
+
+    def __humanname(self, line):
+        """Get a line and
+        Return the IRQ name, alias or number (choose the best for human)
+
+        IRQ line samples:
+        1:      44487        341         44         72   IO-APIC   1-edge      i8042
+        LOC:   33549868   22394684   32474570   21855077   Local timer interrupts
+        """
+        splitted_line = line.split()
+        irq_line = splitted_line[0].replace(':', '')
+        if irq_line.isdigit():
+            # If the first column is a digit, use the alias (last column)
+            irq_line += '_{}'.format(splitted_line[-1])
+        return irq_line
+
+    def __sum(self, line):
+        """Get a line and
+        Return the IRQ sum number
+
+        IRQ line samples:
+        1:      44487        341         44         72   IO-APIC   1-edge      i8042
+        LOC:   33549868   22394684   32474570   21855077   Local timer interrupts
+        """
+        splitted_line = line.split()
+        # sum interrupts on all CPUs
+        return sum(map(int, splitted_line[1:(self.cpu_number + 1)]))
+
+    def __update(self):
+        """
+        Load the IRQ file and update the internal dict
+        """
+
+        self.reset()
+
+        if not os.path.exists(self.IRQ_FILE):
+            # Correct issue #947: IRQ file do not exist on OpenVZ container
+            return self.stats
+
+        with open(self.IRQ_FILE) as irq_proc:
+            time_since_update = getTimeSinceLastUpdate('irq')
+            # Read the header
+            self.__header(irq_proc.readline())
+            # Read the rest of the lines (one line per IRQ)
+            for line in irq_proc.readlines():
+                irq_line = self.__humanname(line)
+                current_irqs = self.__sum(line)
+                irq_rate = int(
+                    current_irqs -
+                    self.lasts.get(irq_line) if self.lasts.get(irq_line) else 0 //
+                    time_since_update)
+                irq_current = {
+                    'irq_line': irq_line,
+                    'irq_rate': irq_rate,
+                    'key': self.get_key(),
+                    'time_since_update': time_since_update
+                }
+                self.stats.append(irq_current)
+                self.lasts[irq_line] = current_irqs
+
+        return self.stats
