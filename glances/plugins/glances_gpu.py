ec27ef53d47b7db41ce3ed89ae63b0e0b506efaf
nicolargo <nicolas@nicolargo.com>
2016-12-17 09:53:06 +0100

    Correct unitary tests issues. Add some PEP8 tips.

--- glances/plugins/glances_gpu.py
+++ glances/plugins/glances_gpu.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Glances.
 #
-# Copyright (C) 2015 Kirby Banman <kirby.banman@gmail.com>
+# Copyright (C) 2016 Kirby Banman <kirby.banman@gmail.com>
 #
 # Glances is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -17,20 +17,23 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""NVIDIA plugin."""
+"""GPU plugin (limited to NVIDIA chipsets)"""
 
 from glances.logger import logger
 from glances.plugins.glances_plugin import GlancesPlugin
 
 try:
-    from pynvml import *
+    import pynvml
 except ImportError:
-    logger.info("Could not import pynvml.  NVIDIA stats will not be collected.")
+    logger.debug("Could not import pynvml.  NVIDIA stats will not be collected.")
+    gpu_nvidia_tag = False
+else:
+    gpu_nvidia_tag = True
 
 
 class Plugin(GlancesPlugin):
 
-    """Glances NVIDIA  plugin.
+    """Glances GPU plugin (limited to NVIDIA chipsets).
 
     stats is a list of dictionaries with one entry per GPU
     """
@@ -39,30 +42,34 @@ class Plugin(GlancesPlugin):
         """Init the plugin"""
         super(Plugin, self).__init__(args=args)
 
-        try:
-            nvmlInit()
-            self.nvml_ready = True
-            self.device_handles = self.get_device_handles()
-            self.devices_ready
-        except Exception:
-            logger.info("pynvml could not be initialized.")
-            self.nvml_ready = False
+        # Init the NVidia API
+        self.init_nvidia()
 
+        # We want to display the stat in the curse interface
+        # !!! TODO: Not implemented yeat
         self.display_curse = False
 
+        # Init the stats
         self.reset()
-
-        if self.input_method == 'local':
-            # Update stats
-            self.stats = self.get_stats()
-        elif self.input_method == 'snmp':
-            # Update stats using SNMP
-            # Not avalaible
-            pass
 
     def reset(self):
         """Reset/init the stats."""
         self.stats = []
+
+    def init_nvidia(self):
+        """Init the NVIDIA API"""
+        if not gpu_nvidia_tag:
+            self.nvml_ready = False
+
+        try:
+            pynvml.nvmlInit()
+            self.device_handles = self.get_device_handles()
+            self.nvml_ready = True
+        except Exception:
+            logger.debug("pynvml could not be initialized.")
+            self.nvml_ready = False
+
+        return self.nvml_ready
 
     @GlancesPlugin._log_result_decorator
     def update(self):
@@ -70,7 +77,7 @@ class Plugin(GlancesPlugin):
 
         self.reset()
 
-        if not self.devices_ready:
+        if not self.nvml_ready:
             return self.stats
 
         if self.input_method == 'local':
@@ -92,9 +99,10 @@ class Plugin(GlancesPlugin):
         """
         Returns a list of NVML device handles, one per device.  Can throw NVMLError.
         """
-        return [nvmlDeviceGetHandleByIndex(i) for i in range(0, nvmlDeviceGetCount())]
+        return [pynvml.nvmlDeviceGetHandleByIndex(i) for i in range(0, pynvml.nvmlDeviceGetCount())]
 
     def get_stats(self):
+        """Get GPU stats"""
         stats = []
         for index, device_handle in enumerate(self.device_handles):
             device_stats = {}
@@ -108,31 +116,37 @@ class Plugin(GlancesPlugin):
         return stats
 
     def get_device_name(self, device_handle):
+        """Get GPU device name"""
         try:
-            return nvmlDeviceGetName(device_handle)
-        except NVMlError:
+            return pynvml.nvmlDeviceGetName(device_handle)
+        except pynvml.NVMlError:
             return "NVIDIA GPU"
 
     def get_memory_percent(self, device_handle):
+        """Get GPU device memory consumption in percent"""
         try:
-            return nvmlDeviceGetUtilizationRates(device_handle).memory
-        except NVMLError:
+            return pynvml.nvmlDeviceGetUtilizationRates(device_handle).memory
+        except pynvml.NVMLError:
             try:
-                memory_info = nvmlDeviceGetMemoryInfo(device_handle)
+                memory_info = pynvml.nvmlDeviceGetMemoryInfo(device_handle)
                 return memory_info.used * 100 / memory_info.total
-            except NVMLError:
-                return -1
+            except pynvml.NVMLError:
+                return None
 
     def get_processor_percent(self, device_handle):
+        """Get GPU device CPU consumption in percent"""
         try:
-            return nvmlDeviceGetUtilizationRates(device_handle).gpu
-        except NVMLError:
-            return -1
+            return pynvml.nvmlDeviceGetUtilizationRates(device_handle).gpu
+        except pynvml.NVMLError:
+            return None
 
     def exit(self):
-        super(NvidiaPlugin, self).exit(args=args)
+        """Overwrite the exit method to close the GPU API"""
         if self.nvml_ready:
             try:
-                nvmlShutdown()
-            except Exception:
-                logger.warn("pynvml failed to shut down correctly.")
+                pynvml.nvmlShutdown()
+            except Exception as e:
+                logger.debug("pynvml failed to shutdown correctly ({})".format(e))
+
+        # Call the father exit method
+        super(Plugin, self).exit()
