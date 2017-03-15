e1582d06d3cdc18d2f1f444ddc0712422fd9fd42
nicolargo <nicolas@nicolargo.com>
2017-03-15 21:14:59 +0100

    FreeBSD: If oid 'hw.acpi.battery' not present, Glances wont start #1055

--- glances/plugins/glances_sensors.py
+++ glances/plugins/glances_sensors.py
@@ -224,23 +224,28 @@ class GlancesGrabSensors(object):
     def __init__(self):
         """Init sensors stats."""
         # Temperatures
+        self.initok = False
+        self.stemps = {}
         try:
             # psutil>=5.1.0 is required
             self.stemps = psutil.sensors_temperatures()
         except AttributeError:
             logger.warning("PsUtil 5.1.0 or higher is needed to grab temperatures sensors")
-            self.initok = False
-            self.stemps = {}
+        except OSError as e:
+            # FreeBSD: If oid 'hw.acpi.battery' not present, Glances wont start #1055
+            logger.error("Can not grab temperatures sensors ({})".format(e))
         else:
             self.initok = True
 
         # Fans
+        self.sfans = {}
         try:
             # psutil>=5.2.0 is required
             self.sfans = psutil.sensors_fans()
         except AttributeError:
             logger.warning("PsUtil 5.2.0 or higher is needed to grab fans sensors")
-            self.sfans = {}
+        except OSError as e:
+            logger.error("Can not grab fans sensors ({})".format(e))
 
         # Init the stats
         self.reset()
