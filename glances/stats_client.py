f95520fd4b88a6f804075ccba5e0063e9c684ab5
nicolargo <nicolas@nicolargo.com>
2017-04-10 15:26:11 +0200

    Improve error message as asked in issue #1078

--- glances/stats_client.py
+++ glances/stats_client.py
@@ -45,13 +45,18 @@ class GlancesStatsClient(GlancesStats):
         header = "glances_"
         for item in input_plugins:
             # Import the plugin
-            plugin = __import__(header + item)
-            # Add the plugin to the dictionary
-            # The key is the plugin name
-            # for example, the file glances_xxx.py
-            # generate self._plugins_list["xxx"] = ...
-            logger.debug("Server uses {} plugin".format(item))
-            self._plugins[item] = plugin.Plugin(args=self.args)
+            try:
+                plugin = __import__(header + item)
+            except ImportError:
+                # Server plugin can not be imported from the client side
+                logger.error("Can not import {} plugin. Please upgrade your Glances client/server version.".format(item))
+            else:
+                # Add the plugin to the dictionary
+                # The key is the plugin name
+                # for example, the file glances_xxx.py
+                # generate self._plugins_list["xxx"] = ...
+                logger.debug("Server uses {} plugin".format(item))
+                self._plugins[item] = plugin.Plugin(args=self.args)
         # Restoring system path
         sys.path = sys_path
 
