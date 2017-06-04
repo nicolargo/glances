8747df4d2321ba2dc7f387a88f6f4f499e2213f6
Floran Brutel <f.brutel@hexanet.fr>
2017-06-04 19:22:46 +0200

    Re-implement hotkets system in the webui

--- glances/outputs/static/js/components/plugin-process/controller.js
+++ glances/outputs/static/js/components/plugin-process/controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function GlancesPluginProcessController(ARGUMENTS) {
+function GlancesPluginProcessController(ARGUMENTS, hotkeys) {
     var vm = this;
     vm.arguments = ARGUMENTS;
 
@@ -18,4 +18,67 @@ function GlancesPluginProcessController(ARGUMENTS) {
             }
         }
     };
+
+    // a => Sort processes automatically
+    hotkeys.add({
+        combo: 'a',
+        callback: function () {
+          vm.sorter.column = "cpu_percent";
+          vm.sorter.auto = true;
+        }
+    });
+
+    // c => Sort processes by CPU%
+    hotkeys.add({
+        combo: 'c',
+        callback: function () {
+          vm.sorter.column = "cpu_percent";
+          vm.sorter.auto = false;
+        }
+    });
+
+    // m => Sort processes by MEM%
+    hotkeys.add({
+        combo: 'm',
+        callback: function () {
+          vm.sorter.column = "memory_percent";
+          vm.sorter.auto = false;
+        }
+    });
+
+    // u => Sort processes by user
+    hotkeys.add({
+        combo: 'u',
+        callback: function () {
+          vm.sorter.column = "username";
+          vm.sorter.auto = false;
+        }
+    });
+
+    // p => Sort processes by name
+    hotkeys.add({
+        combo: 'p',
+        callback: function () {
+          vm.sorter.column = "name";
+          vm.sorter.auto = false;
+        }
+    });
+
+    // i => Sort processes by I/O rate
+    hotkeys.add({
+        combo: 'i',
+        callback: function () {
+          vm.sorter.column = ['io_read', 'io_write'];
+          vm.sorter.auto = false;
+        }
+    });
+
+    // t => Sort processes by time
+    hotkeys.add({
+        combo: 't',
+        callback: function () {
+          vm.sorter.column = "timemillis";
+          vm.sorter.auto = false;
+        }
+    });
 }
