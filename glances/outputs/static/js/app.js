ae135e9867e101417ca01f028456bce62dd17c05
Floran Brutel <f.brutel@hexanet.fr>
2017-06-04 19:22:46 +0200

    Re-implement hotkets system in the webui

--- glances/outputs/static/js/app.js
+++ glances/outputs/static/js/app.js
@@ -1,7 +1,12 @@
-var glancesApp = angular.module('glancesApp', ['ngRoute', 'glances.config', 'fps.hotkeys'])
+var glancesApp = angular.module('glancesApp', ['ngRoute', 'glances.config', 'cfp.hotkeys'])
 
 .value('CONFIG', {})
 .value('ARGUMENTS', {})
+
+.config(function(hotkeysProvider) {
+  hotkeysProvider.useNgRoute = false;
+  hotkeysProvider.includeCheatSheet = false;
+})
 
 .run(function($rootScope) {
       $rootScope.title = "Glances";
