9bf86aee81b9d341b8f0ad678f661aab865fabec
Tieu-Philippe KHIM <khim.tieu.philippe@gmail.com>
2018-02-19 10:47:52 +0100

    adding webpack configuration to support less

--- glances/outputs/static/webpack.config.js
+++ glances/outputs/static/webpack.config.js
@@ -13,6 +13,26 @@ module.exports = {
     module: {
         loaders: [
             {
+                test: /\.scss$/,
+                use: [{
+                    loader: 'style-loader',
+                }, {
+                    loader: 'css-loader',
+                }, {
+                    loader: 'sass-loader',
+                }]
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: 'style-loader',
+                }, {
+                    loader: 'css-loader',
+                }, {
+                    loader: 'less-loader',
+                }]
+            },
+            {
                 test: /\.css$/,
                 loader: 'style-loader!css-loader',
             },
