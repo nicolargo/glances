316d693c97ed33296590383529f4601340b03bb9
nicolargo <nicolas@nicolargo.com>
2019-01-19 16:41:03 +0100

    Glances 3.1.0

--- glances/__init__.py
+++ glances/__init__.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Glances.
 #
-# Copyright (C) 2018 Nicolargo <nicolas@nicolargo.com>
+# Copyright (C) 2019 Nicolargo <nicolas@nicolargo.com>
 #
 # Glances is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -27,7 +27,7 @@ import signal
 import sys
 
 # Global name
-__version__ = '3.1.0_beta'
+__version__ = '3.1.0'
 __author__ = 'Nicolas Hennion <nicolas@nicolargo.com>'
 __license__ = 'LGPLv3'
 
