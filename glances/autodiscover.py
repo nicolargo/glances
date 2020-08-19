38a737fdfd443616cf8ffdb7b2cce0e15178d1ce
nicolargo <nicolas@nicolargo.com>
2020-08-19 10:09:50 +0200

    Can't start server: unexpected keyword argument 'address' #1693

--- glances/autodiscover.py
+++ glances/autodiscover.py
@@ -209,10 +209,23 @@ class GlancesAutoDiscoverClient(object):
             address_family = socket.getaddrinfo(zeroconf_bind_address, args.port)[0][0]
 
             # Start the zeroconf service
-            self.info = ServiceInfo(
-                zeroconf_type, '{}:{}.{}'.format(hostname, args.port, zeroconf_type),
-                address=socket.inet_pton(address_family, zeroconf_bind_address),
-                port=args.port, weight=0, priority=0, properties={}, server=hostname)
+            try:
+                self.info = ServiceInfo(
+                    zeroconf_type, '{}:{}.{}'.format(
+                        hostname, args.port, zeroconf_type),
+                    address=socket.inet_pton(
+                        address_family, zeroconf_bind_address),
+                    port=args.port, weight=0, priority=0, properties={}, server=hostname)
+            except TypeError:
+                # Manage issue 1663 with breaking change on ServiceInfo method
+                # address (only one address) is replaced by addresses (list of addresses)
+                self.info = ServiceInfo(
+                    zeroconf_type, '{}:{}.{}'.format(
+                        hostname, args.port, zeroconf_type),
+                    addresses=[socket.inet_pton(
+                        address_family, zeroconf_bind_address)],
+                    port=args.port, weight=0, priority=0, properties={}, server=hostname)
+
             try:
                 self.zeroconf.register_service(self.info)
             except socket.error as e:
