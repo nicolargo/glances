1fc09356779ef948123fbe14d87f4cdb2cb97690
nicolargo <nicolas@nicolargo.com>
2021-01-23 11:07:36 +0100

    version 3.1.6

--- setup.py
+++ setup.py
@@ -135,6 +135,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: System :: Monitoring'
     ]
 )
