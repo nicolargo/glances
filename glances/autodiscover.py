680fce41322af4031d25909955e53cbd8433fe18
nicolargo <nicolas@nicolargo.com>
2021-06-19 08:34:41 +0200

    Reindent audodiscover.py file

--- glances/autodiscover.py
+++ glances/autodiscover.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Glances.
 #
-# Copyright (C) 2019 Nicolargo <nicolas@nicolargo.com>
+# Copyright (C) 2021 Nicolargo <nicolas@nicolargo.com>
 #
 # Glances is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
@@ -211,21 +211,30 @@ class GlancesAutoDiscoverClient(object):
             # Start the zeroconf service
             try:
                 self.info = ServiceInfo(
-                    zeroconf_type, '{}:{}.{}'.format(
-                        hostname, args.port, zeroconf_type),
-                    address=socket.inet_pton(
-                        address_family, zeroconf_bind_address),
-                    port=args.port, weight=0, priority=0, properties={}, server=hostname)
+                    zeroconf_type,
+                    '{}:{}.{}'.format(hostname,
+                                      args.port,
+                                      zeroconf_type),
+                    address=socket.inet_pton(address_family, zeroconf_bind_address),
+                    port=args.port,
+                    weight=0,
+                    priority=0,
+                    properties={},
+                    server=hostname)
             except TypeError:
                 # Manage issue 1663 with breaking change on ServiceInfo method
                 # address (only one address) is replaced by addresses (list of addresses)
                 self.info = ServiceInfo(
-                    zeroconf_type, '{}:{}.{}'.format(
-                        hostname, args.port, zeroconf_type),
-                    addresses=[socket.inet_pton(
-                        address_family, zeroconf_bind_address)],
-                    port=args.port, weight=0, priority=0, properties={}, server=hostname)
-
+                    zeroconf_type,
+                    '{}:{}.{}'.format(hostname,
+                                      args.port,
+                                      zeroconf_type),
+                    addresses=[socket.inet_pton(address_family, zeroconf_bind_address)],
+                    port=args.port,
+                    weight=0,
+                    priority=0,
+                    properties={},
+                    server=hostname)
             try:
                 self.zeroconf.register_service(self.info)
             except socket.error as e:
