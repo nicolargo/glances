9529bfdb35b494dc3b570e425dd25992f329379c
nicolargo <nicolas@nicolargo.com>
2022-08-07 17:43:11 +0200

    Only work for the first refresh...

--- glances/plugins/glances_ip.py
+++ glances/plugins/glances_ip.py
@@ -10,6 +10,7 @@
 """IP plugin."""
 
 import threading
+import urllib
 from json import loads
 
 from glances.compat import iterkeys, urlopen, queue
@@ -65,6 +66,13 @@ class Plugin(GlancesPlugin):
         public_ip_disabled = self.get_conf_value("public_ip_disabled", default=self._default_public_ip_disabled)
         self.public_ip_disabled = True if public_ip_disabled == ["True"] else False
 
+        # For the Censys options (see issue #2105)
+        self.public_info = ""
+        self.censys_url = self.get_conf_value("censys_url", default=[None])[0]
+        self.censys_username = self.get_conf_value("censys_username", default=[None])[0]
+        self.censys_password = self.get_conf_value("censys_password", default=[None])[0]
+        self.censys_fields = self.get_conf_value("censys_fields", default=[None])
+
     @GlancesPlugin._check_decorator
     @GlancesPlugin._log_result_decorator
     def update(self):
@@ -77,29 +85,52 @@ class Plugin(GlancesPlugin):
 
         if self.input_method == 'local' and not import_error_tag:
             # Update stats using the netifaces lib
+            # Start with the default IP gateway
             try:
                 default_gw = netifaces.gateways()['default'][netifaces.AF_INET]
             except (KeyError, AttributeError) as e:
-                logger.debug("Cannot grab the default gateway ({})".format(e))
+                logger.debug("Cannot grab default gateway IP address ({})".format(e))
                 return {}
+            else:
+                stats['gateway'] = default_gw[0]
 
+            # Then the private IP address
             try:
                 address = netifaces.ifaddresses(default_gw[1])[netifaces.AF_INET][0]['addr']
                 mask = netifaces.ifaddresses(default_gw[1])[netifaces.AF_INET][0]['netmask']
-
-                time_since_update = getTimeSinceLastUpdate('public-ip')
-                if not self.public_ip_disabled and (
-                    self.stats.get('address') != address or time_since_update > self.public_address_refresh_interval
-                ):
-                    self.public_address = PublicIpAddress().get()
             except (KeyError, AttributeError) as e:
-                logger.debug("Cannot grab IP information: {}".format(e))
+                logger.debug("Cannot grab private IP address ({})".format(e))
+                return {}
             else:
                 stats['address'] = address
                 stats['mask'] = mask
                 stats['mask_cidr'] = self.ip_to_cidr(stats['mask'])
-                stats['gateway'] = default_gw[0]
+
+            # Continue with the public IP address
+            time_since_update = getTimeSinceLastUpdate('public-ip')
+            try:
+                if not self.public_ip_disabled and (
+                    self.stats.get('address') != address
+                    or time_since_update > self.public_address_refresh_interval
+                ):
+                    self.public_address = PublicIpAddress().get()
+            except (KeyError, AttributeError) as e:
+                logger.debug("Cannot grab public IP address ({})".format(e))
+            else:
                 stats['public_address'] = self.public_address
+
+            # Finally the Censys information
+            if (
+                self.public_address
+                and not self.public_ip_disabled
+                and (self.stats.get('address') != address
+                     or time_since_update > self.public_address_refresh_interval)
+            ):
+                self.public_info = PublicIpInfo(self.public_address,
+                                                self.censys_url,
+                                                self.censys_username,
+                                                self.censys_password).get()
+                stats['public_info'] = self.public_info
 
         elif self.input_method == 'snmp':
             # Not implemented yet
@@ -151,6 +182,14 @@ class Plugin(GlancesPlugin):
                 msg = ' Pub '
                 ret.append(self.curse_add_line(msg, 'TITLE'))
                 ret.append(self.curse_add_line(msg_pub))
+            if 'public_info' in self.stats:
+                for f in self.censys_fields:
+                    field = f.split(':')
+                    if len(field) == 1 and field[0] in self.stats['public_info']:
+                        msg = '{}'.format(self.stats['public_info'][field[0]])
+                    elif len(field) == 2 and field[0] in self.stats['public_info'] and field[1] in self.stats['public_info'][field[0]]:
+                        msg = '{}'.format(self.stats['public_info'][field[0]][field[1]])
+                    ret.append(self.curse_add_line(msg))
 
         return ret
 
@@ -210,4 +249,60 @@ class PublicIpAddress(object):
                 else:
                     queue_target.put(loads(response)[key])
             except ValueError:
+                queue_target.put(None)
+
+
+class PublicIpInfo(object):
+    """Get public IP information from Censys online service."""
+
+    def __init__(self, ip, url, username, password, timeout=2):
+        """Init the class."""
+        self.ip = ip
+        self.url = url
+        self.username = username
+        self.password = password
+        self.timeout = timeout
+
+    def get(self):
+        """Return the public IP information returned by one of the online service."""
+        q = queue.Queue()
+
+        t = threading.Thread(target=self._get_ip_public_info, args=(q,
+                                                                    self.ip,
+                                                                    self.url,
+                                                                    self.username,
+                                                                    self.password))
+        t.daemon = True
+        t.start()
+
+        timer = Timer(self.timeout)
+        info = None
+        while not timer.finished() and info is None:
+            if q.qsize() > 0:
+                info = q.get()
+
+        if info is None:
+            return None
+
+        return info
+
+    def _get_ip_public_info(self, queue_target, ip, url, username, password):
+        """Request the url service and put the result in the queue_target."""
+        request_url = "{}/v2/hosts/{}".format(url, ip)
+        try:
+            # Python 3 code only
+            # https://stackoverflow.com/questions/24635064/how-to-use-urllib-with-username-password-authentication-in-python-3/24648149#24648149
+            request = urllib.request.Request(request_url)
+            base64string = urllib.request.base64.b64encode(bytes('%s:%s' % (username, password), 'ascii'))
+            request.add_header("Authorization", "Basic %s" % base64string.decode('utf-8'))
+            result = urllib.request.urlopen(request)
+            response = result.read()
+        except Exception as e:
+            logger.debug("IP plugin - Cannot open URL {} ({})".format(request_url, e))
+            queue_target.put(None)
+        else:
+            try:
+                queue_target.put(loads(response)['result'])
+            except (ValueError, KeyError) as e:
+                logger.debug("IP plugin - Cannot get result field from {} ({})".format(request_url, e))
                 queue_target.put(None)
