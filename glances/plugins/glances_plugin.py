1bed69bc8a26463ee837382b7deeee328e6d3718
nicolargo <nicolas@nicolargo.com>
2022-08-27 11:23:00 +0200

    Make the hide function in sensors section compliant with lower/upercase #1590

--- glances/plugins/glances_plugin.py
+++ glances/plugins/glances_plugin.py
@@ -831,7 +831,7 @@ class GlancesPlugin(object):
         show=sda.*
         """
         # @TODO: possible optimisation: create a re.compile list
-        return any(j for j in [re.match(i, value) for i in self.get_conf_value('show', header=header)])
+        return any(j for j in [re.match(i.lower(), value.lower()) for i in self.get_conf_value('show', header=header)])
 
     def is_hide(self, value, header=""):
         """Return True if the value is in the hide configuration list.
@@ -842,7 +842,7 @@ class GlancesPlugin(object):
         hide=sda2,sda5,loop.*
         """
         # @TODO: possible optimisation: create a re.compile list
-        return any(j for j in [re.match(i, value) for i in self.get_conf_value('hide', header=header)])
+        return any(j for j in [re.match(i.lower(), value.lower()) for i in self.get_conf_value('hide', header=header)])
 
     def is_display(self, value, header=""):
         """Return True if the value should be displayed in the UI"""
