cf76040b566634ee468796d729d42a6fe3253d74
nicolargo <nicolas@nicolargo.com>
2022-11-01 10:08:34 +0100

    Update the docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,6 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-from glances import __version__
 import sys
 import os
 from datetime import datetime
@@ -24,6 +23,9 @@ from datetime import datetime
 
 # Insert Glances' path into the system.
 sys.path.insert(0, os.path.abspath('..'))
+
+# WARNING: Do not move this import before the sys.path.insert() call.
+from glances import __version__
 
 
 # -- General configuration ------------------------------------------------
