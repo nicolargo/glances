440fb972cee88c951e2da4fd6e8d9ad3a62f6ab8
nicolargo <nicolas@nicolargo.com>
2022-12-12 11:00:52 +0100

    Drop UT for Python 3.5 and 3.6 (no more available in Ubuntu 22.04)

--- setup.py
+++ setup.py
@@ -135,13 +135,11 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: System :: Monitoring'
     ]
 )
