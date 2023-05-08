593af7a7ef2c839ea946ec40f8c613a17d12f988
nicolargo <nicolas@nicolargo.com>
2023-05-08 08:45:18 +0200

    Add headers to new containers plugin (new name for dockers)

--- glances/plugins/containers/glances_docker.py
+++ glances/plugins/containers/glances_docker.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Glances.
+#
+# SPDX-FileCopyrightText: 2022 Nicolas Hennion <nicolas@nicolargo.com>
+#
+# SPDX-License-Identifier: LGPL-3.0-only
+#
+
 """Docker Extension unit for Glances' Containers plugin."""
 import time
 
--- glances/plugins/containers/stats_streamer.py
+++ glances/plugins/containers/stats_streamer.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Glances.
+#
+# SPDX-FileCopyrightText: 2022 Nicolas Hennion <nicolas@nicolargo.com>
+#
+# SPDX-License-Identifier: LGPL-3.0-only
+
 import threading
 import time
 
