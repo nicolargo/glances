947e24c8cb6e081d852c261030c4d5cb36512e42
nicolargo <nicolas@nicolargo.com>
2023-12-22 18:55:30 +0100

    Update doc 2

--- glances/outputs/glances_stdout_apidoc.py
+++ glances/outputs/glances_stdout_apidoc.py
@@ -200,7 +200,7 @@ def print_top(stats):
     print(sub_title)
     print('-' * len(sub_title))
     print('')
-    print('Get top 2 processes of the processlist plugin:')
+    print('Get top 2 processes of the processlist plugin::')
     print('')
     print('    # curl {}/processlist/top/2'.format(API_URL))
     print(indent_stat(stats.get_plugin('processlist').get_export()[:2]))
@@ -213,7 +213,7 @@ def print_fields_info(stats):
     sub_title = 'GET item description'
     print(sub_title)
     print('-' * len(sub_title))
-    print('Get item description (human readable) for a specific plugin/item:')
+    print('Get item description (human readable) for a specific plugin/item::')
     print('')
     print('    # curl {}/diskio/read_bytes/description'.format(API_URL))
     print(indent_stat(stats.get_plugin('diskio').get_item_info('read_bytes', 'description')))
@@ -223,7 +223,7 @@ def print_fields_info(stats):
     sub_title = 'GET item unit'
     print(sub_title)
     print('-' * len(sub_title))
-    print('Get item unit for a specific plugin/item:')
+    print('Get item unit for a specific plugin/item::')
     print('')
     print('    # curl {}/diskio/read_bytes/unit'.format(API_URL))
     print(indent_stat(stats.get_plugin('diskio').get_item_info('read_bytes', 'unit')))
