82e1aa9e5ee56281b7b6139bf654df514a63c6d8
nicolargo <nicolas@nicolargo.com>
2024-04-07 10:20:19 +0200

    Refactor GPU plugin for multiple GPU type support #2409

---
+++ glances/plugins/gpu/cards/amd.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Glances.
+#
+# SPDX-FileCopyrightText: 2024 Nicolas Hennion <nicolas@nicolargo.com>
+#
+# SPDX-License-Identifier: LGPL-3.0-only
+#
+
+"""AMD Extension unit for Glances' GPU plugin."""
+
+
+class AmdGPU:
+    """GPU card class."""
+
+    def __init__(self):
+        """Init AMD  GPU card class."""
+        pass
+
+    def exit(self):
+        """Close AMD GPU class."""
+        pass
+
+    def get_device_stats(self):
+        """Get AMD GPU stats."""
+        stats = []
+        return stats
