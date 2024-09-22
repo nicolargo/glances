977cf39270d7776cd2ff38379b40628a5bfa4067
nicolargo <nicolashennion@gmail.com>
2024-09-22 10:15:26 +0200

    Top bar is ok

--- glances/outputs/static/js/components/plugin-mem.vue
+++ glances/outputs/static/js/components/plugin-mem.vue
@@ -2,48 +2,70 @@
     <section id="mem" class="plugin">
         <!-- d-none d-xxl-block -->
         <div class="table-responsive">
-            <table class="table table-sm table-borderless">
-                <thead>
-                    <tr>
-                        <th scope="col">MEM</th>
-                        <td scope="col" class="text-end" :class="getDecoration('percent')">{{ percent }}%</td>
-                        <td scope="col" v-show="active != undefined">
-                            active:
-                        </td>
-                        <td scope="col" v-show="active != undefined">
-                            {{ $filters.bytes(active) }}
-                        </td>
-                    </tr>
-                </thead>
+            <table class="table-sm table-borderless">
                 <tbody>
-                    <tr>
-                        <td scope="row">total:</td>
-                        <td class="text-end">{{ $filters.bytes(total) }}</td>
-                        <td scope="col" v-show="inactive != undefined">
-                            inactive:
+                    <tr class="justify-content-between">
+                        <td scope="col">
+                            <table class="table table-sm table-borderless">
+                                <tbody>
+                                    <tr>
+                                        <th scope="col">MEM</th>
+                                        <td scope="col" class="text-end" :class="getDecoration('percent')">{{ percent }}%</td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="row">total:</td>
+                                        <td class="text-end">{{ $filters.bytes(total) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="row">used:</td>
+                                        <td class="text-end" :class="getDecoration('used')">{{ $filters.bytes(used, 2) }}</td>
+                                    </tr>
+                                    <tr>
+                                        <td scope="row">free:</td>
+                                        <td class="text-end" :class="getDecoration('free')">{{ $filters.bytes(free, 2) }}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </td>
-                        <td scope="col" v-show="inactive != undefined">
-                            {{ $filters.bytes(inactive) }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td scope="row">used:</td>
-                        <td class="text-end" :class="getDecoration('used')">{{ $filters.bytes(used, 2) }}</td>
-                        <td scope="col" v-show="buffers != undefined">
-                            buffers:
-                        </td>
-                        <td scope="col" v-show="buffers != undefined">
-                            {{ $filters.bytes(buffers) }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <td scope="row">free:</td>
-                        <td class="text-end" :class="getDecoration('free')">{{ $filters.bytes(free, 2) }}</td>
-                        <td scope="col" v-show="cached != undefined">
-                            cached:
-                        </td>
-                        <td scope="col" v-show="cached != undefined">
-                            {{ $filters.bytes(cached) }}
+                        <td>
+                            <template class="d-none d-xl-block d-xxl-block">
+                                <table class="table table-sm table-borderless">
+                                    <tbody>
+                                        <tr>
+                                            <td scope="col" v-show="active != undefined">
+                                                active:
+                                            </td>
+                                            <td scope="col" v-show="active != undefined">
+                                                {{ $filters.bytes(active) }}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="col" v-show="inactive != undefined">
+                                                inactive:
+                                            </td>
+                                            <td scope="col" v-show="inactive != undefined">
+                                                {{ $filters.bytes(inactive) }}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="col" v-show="buffers != undefined">
+                                                buffers:
+                                            </td>
+                                            <td scope="col" v-show="buffers != undefined">
+                                                {{ $filters.bytes(buffers) }}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td scope="col" v-show="cached != undefined">
+                                                cached:
+                                            </td>
+                                            <td scope="col" v-show="cached != undefined">
+                                                {{ $filters.bytes(cached) }}
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </template>
                         </td>
                     </tr>
                 </tbody>
