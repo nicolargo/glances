44ae3e5f0b2b5e6763792008fbced494e343e699
Branch Vincent <branchevincent@gmail.com>
2024-10-20 11:32:23 -0700

    Migrate to pyproject.toml

--- glances/__init__.py
+++ glances/__init__.py
@@ -15,11 +15,15 @@ import platform
 import signal
 import sys
 import tracemalloc
+from importlib import metadata
 
 # Global name
 # Version should start and end with a numerical char
 # See https://packaging.python.org/specifications/core-metadata/#version
-__version__ = '4.3.0_dev01'
+try:
+    __version__ = metadata.version("glances")
+except metadata.PackageNotFoundError:
+    __version__ = "0.0.0+unknown"
 __apiversion__ = '4'
 __author__ = 'Nicolas Hennion <nicolas@nicolargo.com>'
 __license__ = 'LGPLv3'
