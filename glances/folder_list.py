f730bf3aa23347e79117f898d253b45eea3e5217
nicolargo <nicolashennion@gmail.com>
2024-12-28 18:27:43 +0100

    Correct an issue with the fs plugin in test_perf

--- glances/folder_list.py
+++ glances/folder_list.py
@@ -119,7 +119,7 @@ class FolderList:
         # Iter upon the folder list
         for i in range(len(self.get())):
             # Update folder size
-            if not self.first_grab and not self.timer_folders[i].finished():
+            if not self.first_grab and i in self.timer_folders and not self.timer_folders[i].finished():
                 continue
             # Set the key (see issue #2327)
             self.__folder_list[i]['key'] = key
@@ -132,7 +132,8 @@ class FolderList:
                     )
                 )
             # Reset the timer
-            self.timer_folders[i].reset()
+            if i in self.timer_folders:
+                self.timer_folders[i].reset()
 
         # It is no more the first time...
         self.first_grab = False
