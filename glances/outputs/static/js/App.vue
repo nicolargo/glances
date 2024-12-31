f3c73c7fe4ff03da3436ef0d4282e921767fc9ec
Ben M <ben.mcdowall@gmail.com>
2024-12-31 10:25:54 +0900

    Update App.vue
    Add tailer

--- glances/outputs/static/js/App.vue
+++ glances/outputs/static/js/App.vue
@@ -117,7 +117,8 @@ import GlancesPluginSystem from './components/plugin-system.vue';
 import GlancesPluginUptime from './components/plugin-uptime.vue';
 import GlancesPluginVms from './components/plugin-vms.vue';
 import GlancesPluginWifi from './components/plugin-wifi.vue';
-
+import GlancesPluginTailer from './components/plugin-tailer.vue';
+    
 import uiconfig from './uiconfig.json';
 
 export default {
