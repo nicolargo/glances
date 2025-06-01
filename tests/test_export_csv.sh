f45900b45086c07753478daae1ed533086fd3185
nicolargo <nicolashennion@gmail.com>
2025-06-01 17:37:21 +0200

    Add test for JSON export

--- tests/test_export_csv.sh
+++ tests/test_export_csv.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# Pre-requisites:
-# - jq
 
 # Exit on error
 set -e
 
-# Run glances with export to InfluxDB, stopping after 10 writes
+# Run glances with export to CSV file, stopping after 10 writes
 # This will run synchronously now since we're using --stop-after
 echo "Glances starts to export system stats to CSV file /tmp/glances.csv (duration: ~ 20 seconds)"
 rm -f /tmp/glances.csv
 ./venv/bin/python -m glances --export csv --export-csv-file /tmp/glances.csv --stop-after 10 --quiet
 
 echo "Checking CSV file..."
-./venv/bin/python ./tests-data/tools/csvcheck.py -i /tmp/glances2.csv -l 9
+./venv/bin/python ./tests-data/tools/csvcheck.py -i /tmp/glances.csv -l 9
