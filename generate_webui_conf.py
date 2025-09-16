f2607bd675ce3417b3bebe656dde23cb8ec29cdb
nicolargo <nicolashennion@gmail.com>
2025-09-16 19:05:38 +0200

    First rush to make the top menu configurable

--- generate_webui_conf.py
+++ generate_webui_conf.py
@@ -2,4 +2,12 @@ import json
 
 from glances.outputs.glances_curses import _GlancesCurses
 
-print(json.dumps({"leftMenu": [p for p in _GlancesCurses._left_sidebar if p != "now"]}, indent=4))
+print(
+    json.dumps(
+        {
+            "topMenu": list(_GlancesCurses._top),
+            "leftMenu": [p for p in _GlancesCurses._left_sidebar if p != "now"],
+        },
+        indent=4,
+    )
+)
