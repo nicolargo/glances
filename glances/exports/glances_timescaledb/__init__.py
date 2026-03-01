7c5fc89772b94d671a9074dd83813b19ea267fba
zfh468 <289657570@qq.com>
2026-03-02 06:33:04 +0800

    fix: Add code comments and try-else logic to ensure connection success is logged

--- glances/exports/glances_timescaledb/__init__.py
+++ glances/exports/glances_timescaledb/__init__.py
@@ -18,6 +18,7 @@ from glances.exports.export import GlancesExport
 from glances.logger import logger
 
 # Define the type conversions for TimescaleDB
+# https://www.postgresql.org/docs/current/datatype.html
 convert_types = {
     'bool': 'BOOLEAN',
     'int': 'BIGINT',
@@ -33,13 +34,18 @@ class Export(GlancesExport):
     """This class manages the TimescaleDB export module."""
 
     def __init__(self, config=None, args=None):
+        """Init the TimescaleDB export IF."""
         super().__init__(config=config, args=args)
 
+        # Mandatory configuration keys (additional to host and port)
         self.db = None
+
+        # Optional configuration keys
         self.user = None
         self.password = None
         self.hostname = None
 
+        # Load the configuration file
         self.export_enable = self.load_conf(
             'timescaledb', mandatories=['host', 'port', 'db'], options=['user', 'password', 'hostname']
         )
@@ -49,7 +55,11 @@ class Export(GlancesExport):
             logger.critical("Missing TimescaleDB config")
             return
 
+        # The hostname is always add as an identifier in the TimescaleDB table
+        # so we can filter the stats by hostname
         self.hostname = self.hostname or node().split(".")[0]
+
+        # Init the TimescaleDB client
         self.client = self.init()
 
     def init(self):
@@ -58,13 +68,16 @@ class Export(GlancesExport):
             return None
 
         try:
+            # See https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
             conn_str = f"host={self.host} port={self.port} dbname={self.db} user={self.user} password={self.password}"
             db = psycopg.connect(conn_str)
-            logger.info(f"Stats will be exported to TimescaleDB server: {self.host}:{self.port}")
-            return db
         except Exception as e:
             logger.critical(f"Cannot connect to TimescaleDB server {self.host}:{self.port} ({e})")
             sys.exit(2)
+        else:
+            logger.info(f"Stats will be exported to TimescaleDB server: {self.host}:{self.port}")
+
+        return db
 
     def normalize(self, value):
         """Normalize the value to be exportable to TimescaleDB."""
@@ -73,6 +86,7 @@ class Export(GlancesExport):
         if isinstance(value, bool):
             return str(value).upper()
         if isinstance(value, (list, tuple)):
+            # Special case for list of one boolean
             if len(value) == 1 and isinstance(value[0], bool):
                 return str(value[0]).upper()
             return ', '.join([f"'{v}'" for v in value])
@@ -106,6 +120,9 @@ class Export(GlancesExport):
         """Update the TimescaleDB export module."""
         if not self.export_enable or self.client is None:
             return False
+
+        # Get all the stats & limits
+        # @TODO: Current limitation with sensors, fs and diskio plugins because fields list is not the same
 
         # Filter out unsupported plugins
         self._last_exported_list = [p for p in self.plugins_to_export(stats) if p not in ['sensors', 'fs', 'diskio']]
